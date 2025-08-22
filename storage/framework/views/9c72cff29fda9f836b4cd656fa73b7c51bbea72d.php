64628f69b002eb2a15ed9f720c166144f156da91
Abhiram Kommanapeddi <21A81A0629@sves.org.in>
2025-08-22 17:26:56 +0530

    Add files via upload

---
+++ storage/framework/views/9c72cff29fda9f836b4cd656fa73b7c51bbea72d.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel ="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    </head>
+    <body class="antialiased">
+        <?php echo e($slot); ?>
+
+    </body>
+</html><?php /**PATH C:\xampp\htdocs\Laravel project\laravel11-crash-course\resources\views/components/layout.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b4d7ee3ec99d50efd9d2f715921c7f395ae4c341.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Index Note</h1>
+</div><?php /**PATH C:\xampp\htdocs\Laravel project\laravel11-crash-course\resources\views/note/index.blade.php ENDPATH**/ ?>
